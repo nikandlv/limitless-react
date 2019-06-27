faddf804d58c66fc79d44cc7641f8723ae04ec99
Nikan Dalvand <nikandalvand@gmail.com>
2019-06-27 10:52:35 +0000

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,32 @@
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+    webpack: function (config) {
+        config.module.rules.push({
+            test: /\.(eot|woff|woff2|ttf)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: false,
+                name: '[name].[ext]'
+              }
+            }
+          })
+          config.module.rules.push({
+            test: /\.(svg|png|jpg|gif)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: false,
+                name: '[name].[ext]'
+              }
+            }
+          })
+        return config
+      },
+    exportPathMap: function() {
+        return {
+          '/': { page: '/' },
+    }
+    },
+    assetPrefix: './',
+})
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import('./index.sass')
+import Example from '../components/Example'
+export default function Index() {
+	return (
+		  <div>
+			<h1>Welcome to NextJS boilerplate</h1>
+			<a href="https://github.com/nikandlv/nextjs-boilerplate">Github repo</a>
+			<Example/>
+		  </div>
+
+	);
+}
