83c440dd5d5e4e852ec9d941d9d8a9cfe66eb828
Nikan Dalvand <nikandalvand@gmail.com>
2019-06-28 19:14:49 +0430

    Added some finishing touches

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -3,25 +3,26 @@ import {useState} from 'react'
 import BodyDecorationLight from "../Wrapper/BodyDecorationLight";
 export default function Footer() {
     const [menuOpen,setMenuOpen] = useState(false)
+    const [active,setActive] = useState(0)
     return (
             <footer>
                 <MenuIcon active={menuOpen} onClick={()=> {
                     setMenuOpen(!menuOpen)
                 }}/>
                 <div className="button-gutter"/>
-                <button className="medium active">
+                <button className={`medium ${active === 0?"active":""}`} onClick={()=>setActive(0)}>
                     Features
                 </button>
-                <button className="medium">
+                <button className={`medium ${active === 1?"active":""}`}  onClick={()=>setActive(1)}>
                     Reviews
                 </button>
-                <button className="medium">
+                <button className={`medium ${active === 2?"active":""}`}  onClick={()=>setActive(2)}>
                     Download now
                 </button>
                 <div className="flex-grow-1" />
                 <div>
-                    <span className="credit-main">Designed by <a href="https://dribbble.com/shots/5310448-Dailyui-003-Landingpage">Victor Niculici</a></span>
-                    <span className="credit-sub">Coded by <a href="https://nikandlv.ir">Nikandlv</a></span>
+                    <span className="credit-main">Designed by <a target="_blank" href="https://dribbble.com/shots/5310448-Dailyui-003-Landingpage">Victor Niculici</a></span>
+                    <span className="credit-sub">Coded by <a target="_blank" href="https://nikandlv.ir">Nikandlv</a></span>
                 </div>
                 <div className={`menu ${(menuOpen?"open":"close")}`}>
                     <span className="level-1"/>
--- components/Icons/MenuIcon.jsx
+++ components/Icons/MenuIcon.jsx
@@ -2,7 +2,7 @@ export default function MenuIcon(props) {
     const {active,onClick} = props
     if(active === false) {
         return (
-            <svg onClick={onClick} id="menu" xmlns="http://www.w3.org/2000/svg" width="22" height="14.956" viewBox="0 0 22 14.956">
+            <svg onClick={onClick}  id="menu" className="rotate-menu close" xmlns="http://www.w3.org/2000/svg" width="22" height="14.956" viewBox="0 0 22 14.956">
                 <rect id="Rectangle_101" data-name="Rectangle 101" width="17.6" height="2.137" rx="1.068" transform="translate(4.4)" fill="#212026"/>
                 <rect id="Rectangle_102" data-name="Rectangle 102" width="22" height="2.137" rx="1.068" transform="translate(0 6.41)" fill="#212026"/>
                 <rect id="Rectangle_103" data-name="Rectangle 103" width="17.6" height="2.137" rx="1.068" transform="translate(0 12.819)" fill="#212026"/>
@@ -11,7 +11,7 @@ export default function MenuIcon(props) {
         }
         else {
             return (
-                <svg onClick={onClick} id="menu" xmlns="http://www.w3.org/2000/svg" width="22" height="14.956" viewBox="0 0 22 14.956">
+                <svg onClick={onClick} className="rotate-menu active" id="menu" xmlns="http://www.w3.org/2000/svg" width="22" height="14.956" viewBox="0 0 22 14.956">
                     <rect id="Rectangle_101" data-name="Rectangle 101" width="17.6" height="2.137" rx="1.068" transform="translate(4.4)" fill="#ffffff"/>
                     <rect id="Rectangle_102" data-name="Rectangle 102" width="22" height="2.137" rx="1.068" transform="translate(0 6.41)" fill="#ffffff"/>
                     <rect id="Rectangle_103" data-name="Rectangle 103" width="17.6" height="2.137" rx="1.068" transform="translate(0 12.819)" fill="#ffffff"/>
